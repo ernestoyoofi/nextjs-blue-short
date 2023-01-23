2ed6270d18ac3470c89b0202e09e44f4ab735a61
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2023-01-23 12:29:35 +0700

    First Commites

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
+echo "→ 1/4 Remove modules and cache build"
 rm -r .next
 rm -r node_modules
 rm package-lock.json
+echo "→ 2/4 Git init"
 git init
 git add .
 git commit -m "First Commites"
 git branch -M main
 git remote add origin https://github.com/ernestoyoofi/nextjs-blue-short.git
+echo "→ 3/4 Push To Repository"
 git push -u origin main
+echo "→ 4/4 Horay 🎉"
