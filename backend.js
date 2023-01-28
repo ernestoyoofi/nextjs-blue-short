510e92b1ce0380c6f8e8833b537757394528d3c0
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2023-01-29 06:59:25 +0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,5 +2,6 @@ import fs from "fs"
 
 module.exports = {
   path: require("process").cwd(),
-  file: fs.readdirSync(`${require("process").cwd()}/pages/`)
+  file: fs.readdirSync(`${require("process").cwd()}/pages/`),
+  env: process.env
 }
